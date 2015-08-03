4421b8eb59b87b0fe36c0ce6e48ea41f1e05bf00
DanielPucAguilar <thedanielclub@gmail.com>
2015-08-02 19:06:26 -0500

    Despliegue DB en Heroku

---
+++ models/models.js
@@ -0,0 +1,44 @@
+var path = require ('path');
+
+//Postgress DATABASE_URL = postgres://user:passwd@host:port/database
+//Sqlite DATABASE_URL = sqlite://:@:/
+var url = process.env.DATABASE_URL.match(/(.*)\:\/\/(.*?)\:(.*)@(.*)\:(.*)\/(.*)/);
+var DB_name = ( url[6]||null);
+var user = (url[2]||null);
+var pwd = (url[3]||null);
+var protocol = (url[1]||null);
+var dialect = (url[1]||null);
+var port = (url[5]||null);
+var host = (url[4]||null);
+var storage = process.env.DATABASE_STORAGE;
+//Cargar modelo ORM
+var Sequelize = require('sequelize');
+//Usar BBDD SQlite
+// Usar BBDD SQlite o Posrtgres:
+var sequelize = new Sequelize(DB_name,user,pwd,
+                              { 
+                                dialect: protocol,
+                                protocol:protocol,
+                                port:port,
+                                host:host, 
+                                storage: storage, // solo sqlite
+                                omitNull:true // solo postgres
+                              }
+                              );
+//importar la definicion de la tabla quiz
+var Quiz = sequelize.import(path.join(__dirname,'quiz'));
+exports.Quiz = Quiz;
+
+sequelize.sync().success(function(){
+	Quiz.count().success(function(count){
+		if(count === 0){
+			Quiz.create({
+				pregunta: 'Capital de Italia',
+				respuesta: 'Roma'
+			})
+		.success(function(){
+			console.log('Base de datos inicializada')
+		});
+		};
+	});
+});
