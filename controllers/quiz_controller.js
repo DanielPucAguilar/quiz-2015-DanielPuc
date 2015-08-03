54dbdc17f095ef2212108eaccc8c020e866c575c
DanielPucAguilar <thedanielclub@gmail.com>
2015-08-02 20:31:34 -0500

    Ejercicio Busqueda

--- controllers/quiz_controller.js
+++ controllers/quiz_controller.js
@@ -27,12 +27,35 @@ exports.answer = function(req,res){
 	} 
 	res.render('quizes/answer',{quiz: req.quiz,respuesta: resultado});
 };
-//get quizes
+//get quizes con busqueda
+exports.index = function(req,res){
+    if (req.query.search){
+         var busqueda = req.query.search.replace(/\s+/g, "%");
+        
+
+         models.Quiz.findAll({where: ["upper(pregunta) like ?", "%" + busqueda.toUpperCase() + "%"],order: 'pregunta ASC'}).then(
+
+        function(quizes){
+            res.render('quizes/index',{quizes:quizes, errors: []});
+        }
+        ).catch(function(error){next(error);})
+        
+    }else{
+        models.Quiz.findAll().then(
+            function(quizes){
+            res.render('quizes/index.ejs',{quizes:quizes,errors: []});
+        }
+        ).catch(function(error) {next(error);});
+       
+    }
+};
+
+/**
 exports.index = function(req,res){
 	models.Quiz.findAll().then(function(quizes){
 		res.render('quizes/index.ejs',{ quizes: quizes});
 	}).catch(function(error){next(error);})
-};
+};**/
 exports.author = function(req,res){
     res.render('author',{autor:'Daniel Puc Aguilar',errors: []});
 };
